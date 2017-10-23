2bff85ad3b94fa81bccbfae367b9ff7796ef2167
prince0203 <mc.prince.0203@gmail.com>
2017-10-23 16:16:07 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# PiMonitor
+[![Build Status](https://travis-ci.org/prince0203/PiMonitor.svg?branch=master)](https://travis-ci.org/prince0203/PiMonitor)
+
+## Install
+
+```sh
+$ yarn
+```
+
+## Usage
+
+```sh
+$ WEBSOCKET_PORT=8081 yarn start
+```
+
---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const exec = require('child_process').exec;
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({
+  port: process.env.WEBSOCKET_PORT,
+  clientTracking: true
+});
+
+setInterval(() => {
+  exec(path.join(__dirname, 'pi-sensors'), (err, stdout, stderr) => {
+    if(err) throw err;
+
+    for(client of server.clients) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(stdout);
+      }
+    }
+  });
+}, 1000)
+
