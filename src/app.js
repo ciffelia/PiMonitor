a628f6b6704f57d5775eb29edf6454b6ac902e61
prince0203 <mc.prince.0203@gmail.com>
2017-10-23 18:02:03 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const path = require('path');
 const exec = require('child_process').exec;
 const WebSocket = require('ws');
 
-const server = new WebSocket.Server({
-  port: process.env.WEBSOCKET_PORT,
+const wss = new WebSocket.Server({
+  port: process.env.PIMONITOR_WEBSOCKET_PORT,
   clientTracking: true
 });
 
@@ -11,7 +11,7 @@ setInterval(() => {
   exec(path.join(__dirname, '..', 'pi-sensors'), (err, stdout, stderr) => {
     if(err) throw err;
 
-    for(client of server.clients) {
+    for(client of wss.clients) {
       if (client.readyState === WebSocket.OPEN) {
         client.send(stdout);
       }
